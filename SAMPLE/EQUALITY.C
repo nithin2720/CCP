28872b4472afed9079511cc0fb222ede135c6ee8
nithin2720 <84637354+nithin2720@users.noreply.github.com>
2021-05-22 22:41:05 +0530

    Add files via upload

---
+++ SAMPLE/EQUALITY.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b;
+printf("\nEnter value of A and B");
+scanf("%d %d", &a, &b);
+if(a==b)
+printf("\nthey are equal");
+else
+printf("\nthey are not equal");
+getch();
+}
